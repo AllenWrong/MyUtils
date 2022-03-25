a1c3aa59838ea044abc0441f0a9ace56682c4951
AllenWrong <884691896@qq.com>
2022-03-25 21:22:46 +0800

    update

--- Paper search/paper_search.py
+++ Paper search/paper_search.py
@@ -4,31 +4,35 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def has_key(paper_name, keys, logic="and"):
+def has_key(paper_name, keys):
+    flag = []
     for key in keys:
-        if logic == "and":
+        label = []
+        if key.__class__ == list:
             for k in key:
                 if paper_name.lower().find(k.lower()) == -1:
-                    return False
-            return True
-        # else:
-        #     for k in key:
-        #         if paper_name.lower().find(k.lower()) != -1:
-        #             return True
-
-            # return False
+                    label.append(False)
+                else:
+                    label.append(True)
+        else:
+            if paper_name.lower().find(key.lower()) == -1:
+                label.append(False)
+            else:
+                label.append(True)
+        flag.append(all(label))
+    return any(flag)
 
 
 def write_to_file(content, file):
     # 数据的存储文件，可能比较费时
-    f = open(file, "a+")
+    f = open(file, "a+", encoding="utf-8")
     print(content, file=f)
     f.close()
 
 
-def get_data(url, paper_src, type, keys, logic):
+def get_data(url, paper_src, type, keys):
     """
-    use beautifulsoup to parse data.
+    use beautiful-soup to parse data.
     """
 
     start_time = time.time()
@@ -44,7 +48,7 @@ def get_data(url, paper_src, type, keys, logic):
     for li in lists:
         # avoid the ',' in content be parsed by csv
         title = '"' + li.find(name="span", attrs={"class":"title"}).text + '"'
-        if has_key(title, keys, logic):
+        if has_key(title, keys):
             paper_num += 1
             write_to_file(paper_src + "," + title, save_file)
 
@@ -69,7 +73,29 @@ if __name__ == '__main__':
         # 'multi-modal-papers': ["multi view", "mutli-view", "multi modal", 'multi-modal'],
         # 'trustworthy-papers': ["trust", "reliable"],
         # 'recommend': ["recommend"]
-        "multi-view-incomplete": [["incomplete"]]
+        # "multi-view-incomplete": [["multi view", "incomplete"],
+        #                           ["multi-view", "incomplete"],
+        #                           ["multi modal", "incomplete"],
+        #                           ['multi-modal', "incomplete"],
+        #                           ["multiview", "incomplete"],
+        #                           ["multimodal","incomplete"]]
+        # "self-supervised": ["self-supervised"]
+        # "Representation-Learning": ["Representation Learning"],
+        # "contrastive": ["contrastive"]
+        # "multi-view-cluster":    [["multi view", "cluster"],
+        #                           ["multi-view", "cluster"],
+        #                           ["multi modal", "cluster"],
+        #                           ['multi-modal', "cluster"],
+        #                           ["multiview", "cluster"],
+        #                           ["multimodal", "cluster"]]
+        "multi-view-classification": [["multi view", "classification"],
+                               ["multi-view", "classification"],
+                               ["multi modal", "classification"],
+                               ['multi-modal', "classification"],
+                               ["multiview", "classification"],
+                               ["multimodal", "classification"]],
+        "Federated-Learning": ["Federated Learning"]
+
     }
 
     for task, keys in task_dict.items():
@@ -79,4 +105,4 @@ if __name__ == '__main__':
 
         print(f" ---- Task: {task} ---- ")
         for paper_src, paper_url, type in urls:
-            get_data(paper_url, paper_src, type, keys, "and")
+            get_data(paper_url, paper_src, type, keys)
