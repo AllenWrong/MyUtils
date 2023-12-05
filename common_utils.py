a17f06fb91eec19cc5c4a302f28e56128058f4ea
”AllenWrong“ <884691896@qq.com>
2023-12-05 12:33:39 +0800

    upload

---
+++ common_utils.py
@@ -0,0 +1,18 @@
+import requests
+import json
+
+
+def get_remote_txt(address, parse_fn=None):
+    """get remove txt file and use parse_fn to parse content."""
+    response = requests.get(address)
+    if response.status_code == 200:
+        if parse_fn is not None:
+            return parse_fn(response.text)
+    else:
+        print("can not accesss!")
+        return None
+
+
+def parse_git_repo(response_text):
+    """parse github reposity txt file"""
+    return json.loads(response_text)['payload']['blob']['rawLines']
