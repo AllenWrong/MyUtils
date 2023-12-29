6f49fdbe57a20d08b00d39fad80c7ff8a7abc13f
AllenWrong <884691896@qq.com>
2023-12-29 17:17:36 +0800

    update

--- dl_exp/parser_util.py
+++ dl_exp/parser_util.py
@@ -1,4 +1,8 @@
 import argparse
+import torch
+import random
+import numpy as np
+
 
 def get_parser():
     parser = argparse.ArgumentParser(description='')
@@ -22,3 +26,11 @@ def get_parser():
     parser.add_argument('--log_dir', type=str, default='./log/demo', help='checkpoint path')
     
     return parser.parse_args()
+
+
+def setup_seed(seed):
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+    torch.backends.cudnn.deterministic = True
--- dl_exp/sava_load.py
+++ dl_exp/sava_load.py
@@ -2,23 +2,40 @@ import os
 import torch
 from torch import nn
 import os
+import json
 
 
-def save_torch_model(model_state, opt_state, sched_state, path_dir):
+def save_torch_model(model_state, opt_state, sched_state, path_dir, cfg: dict):
+    """
+    Args:
+        model_state: could not be None
+        opt_state: could not be None
+        sched_state: None or not.
+        path_dir: directory of ckp path. format `expflag_epoch-{base_epoch}-{cur_epoch}`
+            is recommended.
+    """
     os.makedirs(path_dir, exist_ok=True)
 
     torch.save(model_state, os.path.join(path_dir, 'model.pth'))
     torch.save(opt_state, os.path.join(path_dir, 'opt.pth'))
     if sched_state is not None:
         torch.save(sched_state, os.path.join(path_dir, 'sched.pth'))
+    
+    with open(os.path.join(path_dir, 'config.json'), 'w') as f:
+        json.dump(cfg, f)
+
     print(f'saved model in {path_dir}')
 
 
-def load_torch_model(model: nn.Module, opt, sched, path_dir):
+def load_torch_model(model: nn.Module, opt, sched, path_dir) -> dict:
+    """load state and return checkpoint config"""
     model.load_state_dict(torch.load(os.path.join(path_dir, 'model.pth')))
     if opt is not None:
         opt.load_state_dict(torch.load(os.path.join(path_dir, 'opt.pth')))
     if sched is not None:
         sched.load_state_dict(torch.load(os.path.join(path_dir, 'sched.pth')))
 
+    with open(os.path.join(path_dir, 'config.json'), 'r') as f:
+        config = json.load(f)
     print(f'loaded from {path_dir}')
+    return config
