2afee97ec0a9250c4221a3621b1881fff13eaa7c
AllenWrong <884691896@qq.com>
2024-01-10 16:38:59 +0800

    upload

---
+++ print_utils.py
@@ -0,0 +1,19 @@
+
+def part_print(info):
+    print('='*4, info, '='*4)
+    
+    
+def print_h1(info):
+    print('-', info)
+    
+    
+def print_h2(info):
+    print(' *', info)
+    
+    
+def print_h3(info):
+    print('  +', info)
+    
+    
+def print_h4(info):
+    print('   ~', info)
