be24e24ff3add7f0b52ccd8e8cd065c82bc1e7d0
AllenWrong <884691896@qq.com>
2024-03-12 12:05:30 +0800

    feat: support multi-line intent

--- print_utils.py
+++ print_utils.py
@@ -1,19 +1,57 @@
+import numpy as np
+import pandas as pd 
+
+
+def _process_list(info, prefix):
+    if not isinstance(info, str):
+        info = info.__str__()
+
+    info_list = info.split('\n')
+    if len(info_list) > 1:
+        lines = []
+        for it in info_list:
+            lines.append(prefix + it)
+        print('\n'.join(lines))
+    else:
+        print(prefix + info)
+
 
 def part_print(info):
     print('='*4, info, '='*4)
     
     
 def print_h1(info):
-    print('-', info)
+    _process_list(info, '- ')
     
     
 def print_h2(info):
-    print(' *', info)
+    _process_list(info, ' * ')
     
     
 def print_h3(info):
-    print('  +', info)
+    _process_list(info, '  + ')
     
     
 def print_h4(info):
-    print('   ~', info)
+    _process_list(info, '   ~ ')
+
+
+if __name__ == "__main__":
+    arr = np.array([1,2,3,3,3,1])
+    df = pd.DataFrame(arr)
+
+    part_print('Label Distribution Check')
+    print_h1('y train check')
+    print_h2(df.value_counts())
+    print_h3('label 1 cnt valid')
+    print_h3('label 2 cnt valid')
+
+    print_h1('y valid check')
+    print_h2(df.value_counts())
+    print_h3('label 1 cnt valid')
+    print_h3('label 2 cnt valid')
+
+    print_h1('y test check')
+    print_h2(df.value_counts())
+    print_h3('label 1 cnt valid')
+    print_h3('label 2 cnt valid')
